692ff200df2010393ff6c8bb7c8959f01a236a5e
manik <manik@Manikandan>
2024-11-20 09:26:18 +0530

    Adding Demo Class in yugesh package

---
+++ SimpleMavenProject_1/src/test/java/yugesh/Demo_Test.java
@@ -0,0 +1,17 @@
+package yugesh;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+
+public class Demo_Test 
+{
+	@Test
+	public void Example()
+	{
+		Reporter.log("Example Test Executed",true);
+	}
+	
+
+}
